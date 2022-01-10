13633ead1a74d8269830cf4543c1f0b892998222
Eugene Rudakov <voragomod@icloud.com>
2022-01-10 16:43:22 +0300

    Initial commit

---
+++ NetworkApiRouter/NetworkApiRouter.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkApiRouter.
+FOUNDATION_EXPORT double NetworkApiRouterVersionNumber;
+
+//! Project version string for NetworkApiRouter.
+FOUNDATION_EXPORT const unsigned char NetworkApiRouterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkApiRouter/PublicHeader.h>
